acbd4c6095901de2972bbd16e3a72130c62c1537
rmh-avatar <rmarrero9211@gmail.com>
2023-07-16 21:04:32 -0400

    add app entry point

--- app/src/main/java/io/github/rmhavatar/weatherforecast/MainActivity.kt
+++ app/src/main/java/io/github/rmhavatar/weatherforecast/MainActivity.kt
@@ -10,8 +10,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
 import io.github.rmhavatar.weatherforecast.ui.theme.WeatherForecastTheme
 
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/io/github/rmhavatar/weatherforecast/WeatherForeCastApplication.kt
@@ -0,0 +1,8 @@
+package io.github.rmhavatar.weatherforecast
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+@HiltAndroidApp
+class WeatherForeCastApplication : Application()
