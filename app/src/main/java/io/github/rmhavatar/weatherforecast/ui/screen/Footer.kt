59c70e1cd8ec3514af09ae9e4e608e14d13fdb57
rmh-avatar <rmarrero9211@gmail.com>
2023-07-16 22:21:17 -0400

    add footer compose to ForecastScreen

---
+++ app/src/main/java/io/github/rmhavatar/weatherforecast/ui/screen/Footer.kt
@@ -0,0 +1,42 @@
+package io.github.rmhavatar.weatherforecast.ui.screen
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import io.github.rmhavatar.weatherforecast.R
+
+@Composable
+fun Footer(
+    @DrawableRes gpsIcon: Int,
+    onLocationButtonClick: () -> Unit
+) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Text(text = stringResource(id = R.string.app_name))
+        IconButton(onClick = onLocationButtonClick) {
+            Icon(
+                painter = painterResource(id = gpsIcon),
+                contentDescription = null
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FooterPrev() {
+    Footer(gpsIcon = R.drawable.baseline_location_on_24, onLocationButtonClick = {})
+}
