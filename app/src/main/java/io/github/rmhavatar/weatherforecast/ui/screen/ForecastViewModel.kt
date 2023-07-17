465032dbba39f2bd09db22d8179a730fadbce075
rmh-avatar <rmarrero9211@gmail.com>
2023-07-16 22:36:44 -0400

    add ForecastViewModel

---
+++ app/src/main/java/io/github/rmhavatar/weatherforecast/ui/screen/ForecastViewModel.kt
@@ -0,0 +1,76 @@
+package io.github.rmhavatar.weatherforecast.ui.screen
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import io.github.rmhavatar.weatherforecast.data.api.dto.WeatherResponseData
+import io.github.rmhavatar.weatherforecast.data.prefDataStore.DataStoreManager
+import io.github.rmhavatar.weatherforecast.data.repository.ForecastRepository
+import io.github.rmhavatar.weatherforecast.data.util.ResponseState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ForecastViewModel @Inject constructor(
+    private val forecastRepository: ForecastRepository,
+    private val dataStoreManager: DataStoreManager
+) :
+    ViewModel() {
+    private val _weatherDataFlow: MutableStateFlow<ResponseState<WeatherResponseData>?> =
+        MutableStateFlow(null)
+    val weatherDataFlow = _weatherDataFlow.asStateFlow()
+
+    fun fetchWeatherData(hasLocationPermission: Boolean) {
+        viewModelScope.launch {
+            try {
+                _weatherDataFlow.value = ResponseState.Loading()
+                if (hasLocationPermission) {
+                    forecastRepository.fetchWeatherDataByCoordinates().collect {
+                        _weatherDataFlow.value = it
+                    }
+                } else {
+                    dataStoreManager.getLastSearchedCityName().collect { cityName ->
+                        if (cityName.isNullOrBlank()) {
+                            _weatherDataFlow.value = null
+                        } else {
+                            forecastRepository.fetchWeatherDataByCityName(cityName).collect {
+                                _weatherDataFlow.value = it
+                            }
+                        }
+                    }
+                }
+            } catch (e: Exception) {
+                _weatherDataFlow.value = ResponseState.Error("Unexpected Error")
+            }
+        }
+    }
+
+    fun fetchWeatherDataByCityName(cityName: String) {
+        viewModelScope.launch {
+            try {
+                _weatherDataFlow.value = ResponseState.Loading()
+                forecastRepository.fetchWeatherDataByCityName(cityName).collect {
+                    dataStoreManager.saveLastGeneratedTestPollIdsToDataStore(cityName)
+                    _weatherDataFlow.value = it
+                }
+            } catch (e: Exception) {
+                _weatherDataFlow.value = ResponseState.Error("Unexpected Error")
+            }
+        }
+    }
+
+    fun fetchWeatherDataByCoordinates() {
+        viewModelScope.launch {
+            try {
+                _weatherDataFlow.value = ResponseState.Loading()
+                forecastRepository.fetchWeatherDataByCoordinates().collect {
+                    _weatherDataFlow.value = it
+                }
+            } catch (e: Exception) {
+                _weatherDataFlow.value = ResponseState.Error("Unexpected Error")
+            }
+        }
+    }
+}
